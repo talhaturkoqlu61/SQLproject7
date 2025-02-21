4453cb76e961140f3dc8c7c78fc7254795b09ed9
Talha Türkoğlu <158575414+talhaturkoqlu61@users.noreply.github.com>
2025-02-21 12:36:07 +0300

    SQLproject7

---
+++ SQLproject7.sql
@@ -0,0 +1,14 @@
+--SELECT rating,COUNT(*) FROM film
+--GROUP BY rating;(1.question)
+
+--SELECT replacement_cost,COUNT(*)FROM film
+--GROUP BY replacement_cost
+--HAVING COUNT(*)>50;(2.question)
+
+--SELECT store_id,COUNT(*)FROM customer
+--GROUP BY store_id;(3.question)
+
+--SELECT country_id,COUNT(*)FROM city
+--GROUP BY country_id
+--ORDER BY COUNT (*) DESC
+--LIMIT 1;(4.question)
